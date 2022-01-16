ec1be0c6fda9758ef55defc9b2cebbc2282c1c8f
Sohel Shaikh <shaikhsohel.0112@gmail.com>
2022-01-16 23:56:00 +0530

    Till vid 08

--- src/main/java/com/sohel/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/sohel/BetterReadsDataLoaderApplication.java
@@ -2,20 +2,41 @@ package com.sohel;
 
 import java.nio.file.Path;
 
+import javax.annotation.PostConstruct;
+
+import com.sohel.author.Author;
+import com.sohel.author.AuthorRepository;
+import com.sohel.connection.DataStaxAstraProperties;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-import connection.DataStaxAstraProperties;
-
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsDataLoaderApplication {
 
+	@Autowired
+	AuthorRepository authorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+	}
+
+	@PostConstruct
+	public void start(){
+
+		System.out.println("App started");
+
+		Author author = new Author();
+		author.setId("id");
+		author.setName("name");
+		author.setPersonalName("personalName");
+
+		authorRepository.save(author);
 	}
 
 	@Bean
