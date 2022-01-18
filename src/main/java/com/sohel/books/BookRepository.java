bb75fdcd55193ce0c0f7c6823b37a4f327a843cc
Sohel Shaikh <shaikhsohel.0112@gmail.com>
2022-01-18 23:53:39 +0530

    till 10 done, db created

---
+++ src/main/java/com/sohel/books/BookRepository.java
@@ -0,0 +1,10 @@
+package com.sohel.books;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CassandraRepository<Book, String> {
+
+}
+    
